5699d2429d1283076ea67d33cacc099559762665
BohdanKorniienko <95763842+BohdanKorniienko@users.noreply.github.com>
2021-12-13 15:59:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+const $btn = document.getElementById('btn-kick');
+const $btn1 = document.getElementById('btn-kick1');
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function() {
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+$btn1.addEventListener('click', function() {
+    console.log('button fight 2');
+    
+    changeHP(random(100), character);
+    changeHP(random(100), enemy);
+  
+});
+$btn2.addEventListener('click', function() {
+    console.log('Health');
+    changeHP(-10, character);
+    changeHP(-10, enemy);
+});
+
+function init(){
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+  
+}
+function renderHP(person){
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person){
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP + ' HP';
+ 
+}
+function renderProgressbarHP(person) {
+        person.elProgressbar.style.width = person.damageHP + '%';
+}
+function changeHP(count, person){
+    if (person.damageHP < count){
+        person.damageHP = 0;
+        alert('Бедный' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+        $btn1.disabled = true;
+     
+    }else {
+        person.damageHP -= count;
+    }
+ 
+
+    renderHP(person);
+}
+function random(num){
+    return Math.ceil(Math.random() * num);
+}
+
+init();
